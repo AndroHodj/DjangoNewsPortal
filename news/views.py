784d14dd7a1b78c261b6324683ce0fb1aaa795a1
andro <fffff.ddddd2012@yandex.ru>
2022-10-05 19:23:47 +0300

    Добавлены файлы: custom_tags.py, filters.py, forms.py, news_articles_delete.html, news_edit.html. В моделе Post добавлен метод get_absolute_url. В файле filters.py добавлена фильтрация на главной странице сайта В файле forms.py добавлены формы для создания статей и новостей, так же происходит сравнение полей. В файле custom_tags.py новый таг url_replace, чтобы работала пагинации вместе с фильтрацией на каждой странице В файле news.urls.py добавлены новые url адреса. В файле newspaper.url.py изменено два url адреса В файле settings.py установленно новое приложение django_filters В файле views.py появились 4 новых представления(Create, Update, Delete)

--- news/views.py
+++ news/views.py
@@ -1,17 +1,67 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from .models import Post
+from .forms import NewsForm, ArticlesForm
+from django.urls import reverse_lazy
+from .filters import NewsFilter
+from .resources import news, article
 
 
 class NewsList(ListView):
     model = Post
     ordering = 'heading'
-    template_name = 'news.html'
+    template_name = 'news_list.html'
     context_object_name = 'news'
+    paginate_by = 1
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = NewsFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
 
 
-class OneNews(DetailView):
+class OneDetail(DetailView):
     model = Post
     ordering = 'heading'
-    template_name = 'new.html'
+    template_name = 'news_detail.html'
     context_object_name = 'onenews'
+
+
+class ArticlesCreate(CreateView):
+    form_class = ArticlesForm
+    model = Post
+    template_name = 'news_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.article_or_news = article
+        return super().form_valid(form)
+
+
+class NewsCreate(CreateView):
+    form_class = NewsForm
+    model = Post
+    template_name = 'news_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.article_or_news = news
+        return super().form_valid(form)
+
+
+class NewsDelete(DeleteView):
+    model = Post
+    template_name = 'news_articles_delete.html'
+    success_url = reverse_lazy('news_list')
+
+
+class NewsUpdate(UpdateView):
+    form_class = NewsForm
+    model = Post
+    template_name = 'news_edit.html'
+
 
